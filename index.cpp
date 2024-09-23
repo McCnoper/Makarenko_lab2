89dc722343ce0b55eda44fcf1af5edf6c2b17926
McCnoper <r00mmmaaaa@gmail.com>
2024-09-23 09:00:12 +0300

    Update index.cpp

--- index.cpp
+++ index.cpp
@@ -5,15 +5,22 @@ int main() {
     int a = 1;
     int b = 1;
     int c = 0;
+    int sum = 0;
     cout<<a<<" ";
     cout<<b<<" ";
+    sum=a+b;
     for(int i = 1; i<10; i++){
         c= a+b;
-        if(c<1000)cout<<c<<" ";
+        if(c<1000){cout<<c<<" ";
         a = c;
+        sum+=c;    
+        }
         
         c= a+b;
-        if(c<1000)cout<<c<<" ";
+        if(c<1000){cout<<c<<" ";
         b = c;
+        sum+=c;    
+        }
     }
+    cout<<endl<<"Сумма чисел Фібоначчі до 1000: "<<sum;
 }
